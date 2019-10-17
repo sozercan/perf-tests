a78aadeb88b096245698489657f7d0459951baa8
Sertaç Özercan <sozercan@users.noreply.github.com>
2019-10-16 19:08:55 -0700

    update deployment script

--- deploy_gk.sh
+++ deploy_gk.sh
@@ -7,12 +7,15 @@ if [ $DEPLOY_GATEKEEPER = "true" ]; then
     if [ $DEPLOY_GATEKEEPER_SYNC = "true" ]; then
         kubectl apply -f clusterloader2/testing/gatekeeper/sync.yaml
         sleep 5
-    fi
 
-    if [ $DEPLOY_GATEKEEPER_TEMPLATE = "true" ]; then
-        kubectl apply -f clusterloader2/testing/gatekeeper/allowedrepos-template.yaml
-        sleep 100
-        kubectl apply -f clusterloader2/testing/gatekeeper/allowedrepos-constraint.yaml
-        sleep 30
+        if [ $DEPLOY_GATEKEEPER_TEMPLATE = "true" ]; then
+            kubectl apply -f clusterloader2/testing/gatekeeper/allowedrepos-template.yaml
+            sleep 100
+            kubectl apply -f clusterloader2/testing/gatekeeper/allowedrepos-constraint.yaml
+            sleep 30
+
+            # TODO: multiple templates
+            # TODO: multiple constraints
+        fi
     fi
 fi
