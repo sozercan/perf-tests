000e32017707f09fb33cc1585aa34c5ff29c78c6
Sertaç Özercan <sozercan@users.noreply.github.com>
2019-10-20 15:39:32 -0700

    multiple constraints

--- deploy_gk.sh
+++ deploy_gk.sh
@@ -8,14 +8,24 @@ if [ $DEPLOY_GATEKEEPER = "true" ]; then
         kubectl apply -f clusterloader2/testing/gatekeeper/sync.yaml
         sleep 5
 
+        # TODO: multiple templates
         if [ $DEPLOY_GATEKEEPER_TEMPLATE = "true" ]; then
             kubectl apply -f clusterloader2/testing/gatekeeper/allowedrepos-template.yaml
             sleep 100
-            kubectl apply -f clusterloader2/testing/gatekeeper/allowedrepos-constraint.yaml
-            sleep 120
 
-            # TODO: multiple templates
-            # TODO: multiple constraints
+            i="0"
+            while [ $i -lt $NUMBER_CONSTRAINTS ]
+            do
+                export CONSTRAINT_NAME=repo-$(openssl rand -hex 6)
+
+                envsubst < clusterloader2/testing/gatekeeper/allowedrepos-constraint-template.yaml > clusterloader2/testing/gatekeeper/allowedrepos-constraint.yaml
+
+                kubectl apply -f clusterloader2/testing/gatekeeper/allowedrepos-constraint.yaml
+
+                sleep 10
+
+                i=$[$i+1]
+            done
         fi
     fi
 fi
